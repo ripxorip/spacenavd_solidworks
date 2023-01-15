db16fbecafde5ef794ba0c08236e96881e9d7eee
Philip K. Gisslow <ripxorip@gmail.com>
2023-01-15 14:05:51 +0100

    Adds example csharp implementation

---
+++ examples/c_sharp_from_socket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = "192.168.50.117";
+            Int32 port = 11111;
+            using (TcpClient client = new TcpClient(server, port))
+            {
+                NetworkStream stream = client.GetStream();
+                while (true)
+                {
+                    var data = new Byte[256];
+                    var res = stream.Read(data, 0, 32);
+                    var res_array = new Int32[32/4];
+                    for (int i = 0; i < 32/4; i++)
+                    {
+                        int num = BitConverter.ToInt32(data, i*4);
+                        res_array[i] = num;
+                    }
+
+                    for (int i = 0; i < 32/4; i++)
+                    {
+                        Console.WriteLine(res_array[i]);
+                    }
+                    Console.WriteLine("*****");
+                }
+            }
+        }
+    }
+}
